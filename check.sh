4a670b3ccf34191e2a86ec72b133f00d52d7b062
Dodoslav Novak <dodoslav@gmail.com>
2022-06-02 17:01:20 +0200

    ifix

--- check.sh
+++ check.sh
@@ -13,24 +13,26 @@ if [ "$1" == "" ]
 	fi
 PARAM_MODUL=$1
 
-if [ "$2" == "" ]
-        then
-        echo Chyba nazov senzora!
-	echo
-	echo Toto su moznosti:
-	
-        exit 1
-        fi
-PARAM_SENSOR=$2
-
-
-
 POM=$( cat esp_modules.csv | grep -v ^# | grep \;$PARAM_MODUL\; )
 if [ "$POM" == "" ]
         then
         echo Modul sa v zozname nenasiel!
         exit 1
         fi
+
+MODUL_IP=$( cat esp_modules.csv | grep -v ^# | grep \;$PARAM_MODUL\; | cut -d';' -f3)
+MODUL_ID=$( cat esp_modules.csv | grep -v ^# | grep \;$PARAM_MODUL\; | cut -d';' -f1)
+
+if [ "$2" == "" ]
+        then
+        echo Chyba nazov senzora!
+        echo
+        echo Toto su moznosti:
+        cat esp_sensors.csv | grep ^$MODUL_ID";" | cut -d';' -f2
+
+        exit 1
+        fi
+PARAM_SENSOR=$2
 
 POM=$( cat esp_sensors.csv | grep -v ^# | grep \;$PARAM_SENSOR$ )
 if [ "$POM" == "" ]
@@ -39,9 +41,6 @@ if [ "$POM" == "" ]
         exit 1
         fi
 
-
-MODUL_IP=$( cat esp_modules.csv | grep -v ^# | grep \;$PARAM_MODUL\; | cut -d';' -f3)
-
 ping -c 1 $MODUL_IP 1>/dev/null 2>&1
 if [ "$?" != 0 ]
         then
@@ -49,7 +48,7 @@ if [ "$?" != 0 ]
         exit 1
         fi
 
-wget -O $PARAM_MODUL".html" $MODUL_IP  1>/dev/null 2>&1
+wget --tries=2 --connect-timeout=5 --timeout=5 -O $PARAM_MODUL".html" $MODUL_IP  1>/dev/null 2>&1
 
 cat $PARAM_MODUL".html" | grep -A 1 $PARAM_SENSOR | tail -1
 
--- monitoring.sh
+++ monitoring.sh
@@ -2,9 +2,13 @@
 
 cd "$(dirname "$0")"
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+
+IFS=$'\n'
+
 for MODULE in $( cat esp_modules.csv | grep -v ^# )
 	do
-	MODULE_ID=$( cat esp_modules.csv | grep -v ^# | cut -d";" -f1  )
+	MODULE_ID=$( echo $MODULE | grep -v ^# | cut -d";" -f1  )
 	for SENSOR in $( cat esp_sensors.csv | grep $MODULE_ID )
 		do
 		echo '------------'
@@ -12,5 +16,6 @@ for MODULE in $( cat esp_modules.csv | grep -v ^# )
 		POM=$( ./check.sh $( echo $MODULE | cut -d';' -f 2 ) $( echo $SENSOR | cut -d';' -f 2 ) )
 		echo $POM
 		echo $POM > /tmp/ramdisk/esp_$( echo $MODULE | cut -d';' -f 2 )-$( echo $SENSOR | cut -d';' -f 2 ).txt
+		chmod 755 /tmp/ramdisk/esp_$( echo $MODULE | cut -d';' -f 2 )-$( echo $SENSOR | cut -d';' -f 2 ).txt
 		done	
 	done
